c6a96df03696cb45d04b3f158ae41a6e213d3295
Jakobi832 <jakobi832@gmail.com>
2024-02-27 15:28:27 +1000

    Add SimpleStorage example in foundry

---
+++ foundry-f23/foundry-simpleStorage-f23/script/DeploySimpleStorage.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast(); // send to rpc
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+
+}
